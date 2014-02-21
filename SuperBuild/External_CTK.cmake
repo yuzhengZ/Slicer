a8b9841f2fe3bcbd2935f94c7e9bc4fd3f7fa2ca
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2014-02-21 15:39:27 -0500

    COMP: Support VTK6 in CTK tests
    1) Support VTK6 in CTK tests
    2) use SYSTEM_ZLIB

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/yuzhengZ/CTK.git"
-    GIT_TAG "63cb97cf364e046a65f9f9f80596f6ce79850fd8"
+    GIT_TAG "8c3a6e381db232017c7699730ec474a993d47cd9"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
